8ee5e3e6d655508ee6a2a656ece202df9faf4c33
max <m.d.mihailik@gmail.com>
2024-08-21 23:56:55 +0300

    feat: add dark/light mode button

--- src/components/buttons/DarkLightModeSwitch.tsx
+++ src/components/buttons/DarkLightModeSwitch.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useContext } from "react";
+import { Dispatch, SetStateAction, useContext, useState } from "react";
 import { ApplicationContext } from "../../App";
 
 const DarkLightModeSwitch = ({
@@ -7,17 +7,26 @@ const DarkLightModeSwitch = ({
     setIsDarkMode: Dispatch<SetStateAction<boolean>>;
 }) => {
     const { isDarkMode } = useContext(ApplicationContext)!;
+    const [triggerAnimation, setTriggerAnimation] = useState<boolean>(false);
+
+    const handleToggle = () => {
+        setIsDarkMode((prevState) => !prevState);
+
+        setTriggerAnimation(true);
+        setTimeout(() => setTriggerAnimation(false), 200);
+    };
 
     return (
         <div className="fixed top-[63px] left-[111px]">
             <div
                 className={`${
                     isDarkMode
-                        ? "bg-[url(/images/sunLightDarkModeSwitch.svg)]"
-                        : "bg-[url(/images/moonLightDarkModeSwitch.svg)]"
+                        ? "bg-[url(/images/sunLightDarkModeSwitch.svg)] transition-all delay-[100ms]"
+                        : "bg-[url(/images/moonLightDarkModeSwitch.svg)] transition-all delay-[100ms]"
                 }
-                            w-[35px] h-[35px] hover:cursor-pointer`}
-                onClick={() => setIsDarkMode((prevState) => !prevState)}
+                ${triggerAnimation ? "appear" : ""}
+                w-[35px] h-[35px] hover:cursor-pointer`}
+                onClick={handleToggle}
             ></div>
         </div>
     );
