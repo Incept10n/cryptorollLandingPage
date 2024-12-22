087c2a3cd9531583f45c5d00bc039abdcf868e0a
max <m.d.mihailik@gmail.com>
2024-12-22 15:16:02 +0300

    fix: add parameterized urls

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,10 @@ export const LANGUAGES = [
     { label: "English", code: "en" },
 ];
 
-export const currentURL =
-    import.meta.env.MODE === "development" ? "http://localhost:5173" : "";
+export const currentURL = "http://localhost:5174"; // "http://localhost:4173";
+// import.meta.env.MODE === "development"
+//     ? window.location.origin //"http://172.27.33.20:5173"
+//     : "/landing";
+
+export const HOME_URL = "/landing-page";
+export const WHITEPAPER_URL = `${HOME_URL}/whitepaper`;
