a103fa6b74e6c6857faa502910bc9ab5f817b97e
max <m.d.mihailik@gmail.com>
2024-12-23 14:54:28 +0300

    feat: make some changes

--- src/constants.ts
+++ src/constants.ts
@@ -3,14 +3,17 @@ export const LANGUAGES = [
     { label: "English", code: "en" },
 ];
 
-export const currentURL = import.meta.env.DEV
-    ? "http://localhost:4173/landing-page"
-    : "https://cryptoroll.su/landing-page";
+// export const currentURL = import.meta.env.DEV
+//     ? "http://localhost:4173/landing-page"
+//     : "https://cryptoroll.su/landing-page";
 
 //"http://localhost:5174"; // "http://localhost:4173";
 // import.meta.env.MODE === "development"
 //     ? window.location.origin //"http://172.27.33.20:5173"
 //     : "/landing";
 
-export const HOME_URL = "/landing-page";
+// export const HOME_URL = "/landing-page";
+// export const WHITEPAPER_URL = `${HOME_URL}/whitepaper`;
+
+export const HOME_URL = "";
 export const WHITEPAPER_URL = `${HOME_URL}/whitepaper`;
